be459aa3ff685c5e3390447ff9e3d5210d2cfac5
Adarsh_petr <coe13b003@iiitdm.ac.in>
2016-10-10 12:48:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,16 @@
 console.log('Loaded!');
 
-var element = document.getElementById("text");
+var element = document.getElementById("image-madi");
+
+var marginleft=0;
+
+function moveRight()
+{
+    marginleft=marginleft + 10;
+    element.style.marginLeft = marginleft + 'px';
+}
+
+element.onClick = function(){
+    var interval = setInterval(moveRight,100);
+}
 
